5a62f431d6cde76e11b12838fa1c4670f99332cd
EPICSLASH999 <58922311+EPICSLASH999@users.noreply.github.com>
2023-02-02 17:52:35 -0600

    Add files via upload
    Version 1.5
    Fecha 01 / 12 / 22
    ----------------------
    
    --Agregado de Comandos de TORTA y ENEMIGO
    --El primer turno ahora es aleatorio (anteriormente empezaba el que invitaba a jugar)
    --casillasDisponibles de "TableroO" volvio a un valor de 30

---
+++ V5/ServidorB/src/tableros/TableroT.java
@@ -0,0 +1,94 @@
+
+package tableros;
+
+public class TableroT {
+    
+    final String[] filas = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+    final String[] columnas = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    
+    String[][] tablero = new String[10][10];
+    
+    final String oceano = "-";
+    
+    final String tiroAcertado = "O";
+    final String tiroFallido = "X";
+    
+    public TableroT(){
+        construirTablero();
+    }
+    
+    public void Tiro(String coord, boolean acertado){
+        int col = calcularColumna(coord) - 1;
+        int fil = calcularFila(coord);
+        
+        if (acertado){
+            tablero[fil][col] = tiroAcertado;
+        } else{
+            tablero[fil][col] = tiroFallido;
+        }
+        
+    }
+    
+    public int calcularFila(String coord) {
+        int c = 0;
+        String fila = String.valueOf(coord.charAt(0));
+        for (String s : filas) {
+            if (filas[c].equals(fila)) {
+                break;
+            }
+            c++;
+        }
+
+        return c;
+    }
+
+    public int calcularColumna(String coord) {
+        String n = "";
+        if (coord.length() == 3) {
+            n = String.valueOf(coord.charAt(1)) + String.valueOf(coord.charAt(2));
+        } else if (coord.length() == 2) {
+            n = String.valueOf(coord.charAt(1));
+        }
+        return Integer.parseInt(n);
+    }
+    
+    public void construirTablero() {
+
+        for (int x = 0; x < filas.length; x++) {
+            for (int y = 0; y < columnas.length; y++) {
+                tablero[x][y] = oceano;
+
+            }
+        }
+
+    }
+    
+    public String getTablero() {
+        String margen = "               ";
+        
+        String espaciado = "  ";
+
+        String cadena = "";
+        
+        cadena += margen + "* * * * * MAPA DE TIRO * * * * *ln";
+        cadena += espaciado + " " + margen;
+        //Esto imprime los numeros de arriba (columnas)
+        for (String columna : columnas) {
+            cadena += columna + espaciado;
+        }
+        cadena += "ln";
+
+        for (int x = 0; x < filas.length; x++) {
+            cadena += margen + filas[x] + espaciado;
+            for (int y = 0; y < columnas.length; y++) {
+                cadena += tablero[x][y] + espaciado;
+                
+            }
+            cadena += "ln";
+        }
+        cadena += "ln";
+
+        return cadena;
+    }
+
+}
